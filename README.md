e6a673cd18d4ccfc6dda5122a9614aceffc53a27
Xuhaojun <xuhaojuntw@gmail.com>
2017-05-29 18:36:50 +0800

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple chinese n-gram
---
+++ ngram.py
@@ -0,0 +1,20 @@
+from pyspark import SparkContext
+
+# n=2, line="1234"; result=["12", "23", "34", "4"]
+def nGramChineseSplit(n, line):
+     return list(map(lambda anchorPoint: line[anchorPoint:anchorPoint + n], \
+                     range(1, len(line))))
+
+def nGram(sc, numGram):
+    text_file = sc.textFile("README.md")
+    counts = text_file.flatMap(lambda line: nGramChineseSplit(numGram, line)) \
+                .map(lambda word: (word, 1)) \
+                .reduceByKey(lambda totalCount, count: totalCount + count) \
+                .sortBy(lambda t: -t[1])
+    return counts
+
+sc = SparkContext("local", "N-Gram")
+
+result = nGram(sc, 2)
+
+result.saveAsTextFile("ngram_result")
