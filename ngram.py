e42cbd9d5abd59edfa48c4aafebc36aa813bbcd8
Xuhaojun <xuhaojuntw@gmail.com>
2017-05-29 18:43:46 +0800

    Update.

--- ngram.py
+++ ngram.py
@@ -3,10 +3,10 @@ from pyspark import SparkContext
 # n=2, line="1234"; result=["12", "23", "34", "4"]
 def nGramChineseSplit(n, line):
      return list(map(lambda anchorPoint: line[anchorPoint:anchorPoint + n], \
-                     range(1, len(line))))
+                     range(0, len(line))))
 
 def nGram(sc, numGram):
-    text_file = sc.textFile("README.md")
+    text_file = sc.textFile("example-data.txt")
     counts = text_file.flatMap(lambda line: nGramChineseSplit(numGram, line)) \
                 .map(lambda word: (word, 1)) \
                 .reduceByKey(lambda totalCount, count: totalCount + count) \
